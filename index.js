90858f7cb11ae1bfac7941da836b74a575cc992c
Iago Oliveira <iagooliveira1806@gmail.com>
2024-04-17 13:04:26 -0300

    continuação do projeto

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const mysql2 = require('mysql2');
 //express
 const app = express();
 
+//cofiguração do middleware para verificar a solicitação com o tipo de conteudo 
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
 //configura o middlaware para analisar solicitações com o tipo de conteudo
 app.use(express.json());
 
@@ -14,17 +21,17 @@ app.set("view engine", "handlebars");
 
 
 //configurando a primeira rota 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     //.res.send('mandando info na tela');
     res.render('home')//renderizando a home
 });
 
 //listando os produtos 
-app.get('/lista', (req, res)=>{
+app.get('/lista', (req, res) => {
     const sql = 'SELECT * FROM Produtos';
-//função de conexao MySQL
-    conn.query(sql, function(err, data){
-        if(err) {
+    //função de conexao MySQL
+    conn.query(sql, function (err, data) {
+        if (err) {
             console.log(err);
             return;
         }
@@ -37,22 +44,41 @@ app.get('/lista', (req, res)=>{
 
 });
 
+//cadastrando
+app.post("/listas/insertProdutos", (req, res) => {
+    const produto = req.body.Produto;
+    const preco = req.body.Preco;
+    const descricao = req.body.Descricao;
+
+    //query do sql para cadastro
+    const sql = `INSERT INTO produtos(Produto, Preco, Descricao) values ('${produto}', ${preco},'${descricao}')`
+
+    conn.query(sql, function (err) {
+        if (err) {
+            console.log('erro imediato', err)
+            return false;
+        }
+        res.redirect("/lista");
+    });
+
+})
+
 //conectando com banco de dados 
 const conn = mysql2.createConnection({
     host: 'localhost',
     user: 'root',
-    password:'root',
-    database: 'Mercadinho'  
+    password: 'root',
+    database: 'Mercadinho'
 });
 
 //configurando banco de dados
-    conn.connect(function (err){
-        if(err){
-            console.log(err);
-        }
-    });
+conn.connect(function (err) {
+    if (err) {
+        console.log(err);
+    }
+});
 
 //porta e executando o projeto 
-console.log('conectou ao MySQL'); 
+console.log('conectou ao MySQL');
 
 app.listen(3000);
